b1327a441aa7e396eca41b9029ce0c43847db10a
Rinku Kundu <142101116+Rinkugithubn@users.noreply.github.com>
2024-09-28 11:21:42 +0530

    Add files via upload

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+class database{
+protected $connection;
+
+function setconnection(){
+    try{
+        $this->connection=new PDO("mysql:host=localhost; dbname=library_managment","root","");
+        // echo "Done";
+    }catch(PDOException $e){
+        echo "Error";
+        //die();
+
+    }
+}
+
+}
